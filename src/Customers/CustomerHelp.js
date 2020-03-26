c7fe23600172b9b9560bdfd8e94ef611e1ecbe39
Isaac orija <isaac.orija@gmail.com>
2020-03-26 13:09:32 +0100

    done with customer help

--- src/Customers/CustomerHelp.js
+++ src/Customers/CustomerHelp.js
@@ -1,7 +1,12 @@
 import React from 'react'
-import Search from './Search'
+// import Search from './Search'
 import {Link} from 'react-router-dom'
 import OrderIcon from '../assets/images/box.png'
+import Payments from '../assets/images/payment-method.png'
+import Delivery from '../assets/images/package.png'
+import Returns from  '../assets/images/exchange.png'
+import Stock from '../assets/images/tshirt.png'
+import Support from '../assets/images/support.png'
 
 class CustomerHelp extends React.Component{
     constructor(props){
@@ -10,19 +15,47 @@ class CustomerHelp extends React.Component{
     render(){
         return(
             <React.Fragment>
-                <Search/>
-                <div className='container-fluid'>
+                {/* <Search/> */}
+                <div className='container-fluid help-components'>
                  <div className="top-row flex-containers">
-                    <div className="">
-                        <img src={OrderIcon} className="img-fluid" alt="icon for order issues"/>
+                    <div className="order-box">
+                       <a href="">
+                       <img src={OrderIcon} className="img-fluid help-icons" alt="icon for order issues"/>
+                        <p>Order Issues</p>
+                       </a>
                     </div>
-                    <div className="">2</div>
-                    <div className="">3</div>
+                    <div className="order-box">
+                    <a href="">
+                       <img src={Payments} className="img-fluid help-icons" alt="icon for payment issues"/>
+                        <p>Payments & Discounts</p>
+                       </a>
+                    </div>
+                    <div className="order-box">
+                    <a href="">
+                       <img src={Delivery} className="img-fluid help-icons" alt="icon for payment issues"/>
+                        <p>Delivery Issues</p>
+                       </a>
+                    </div>
                  </div>
                  <div className="bottom-row flex-containers">
-                 <div className="">1</div>
-                 <div className="">2</div>
-                 <div className="">3</div>
+                 <div className="order-box">
+                 <a href="">
+                       <img src={Returns} className="img-fluid help-icons" alt="icon for return & refunds"/>
+                        <p>Returns & Refunds</p>
+                       </a>
+                 </div>
+                 <div className="order-box">
+                 <a href="">
+                       <img src={Stock} className="img-fluid help-icons" alt="icon for Product & Stock issues"/>
+                        <p>Product & Stocks</p>
+                       </a>
+                 </div>
+                 <div className="order-box">
+                 <a href="">
+                       <img src={Support} className="img-fluid help-icons" alt="icon for customer support"/>
+                        <p>Customer Support</p>
+                       </a>
+                 </div>
                  </div>
                 </div>
             </React.Fragment>
