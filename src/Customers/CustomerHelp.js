3f33d43fe3dee8c0617a8415245909745b3c25d5
Isaac orija <isaac.orija@gmail.com>
2020-03-26 11:35:57 +0100

    removed Icons from ant

--- src/Customers/CustomerHelp.js
+++ src/Customers/CustomerHelp.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Search from './Search'
 import {Link} from 'react-router-dom'
+import OrderIcon from '../assets/images/box.png'
 
 class CustomerHelp extends React.Component{
     constructor(props){
@@ -11,27 +12,18 @@ class CustomerHelp extends React.Component{
             <React.Fragment>
                 <Search/>
                 <div className='container-fluid'>
-                   <div className ='row'>
-                     <div className='col-md-4 col-lg-4'>
-                        <h3><Link to ='/'>Order Issues</Link></h3>
-                     </div>
-                     <div className='col-md-4 col-lg-4'>
-                        <h3><Link to ='/'>Payment & Discounts</Link></h3>
-                     </div>
-                     <div className='col-md-4 col-lg-4'>
-                        <h3><Link to ='/'>Delivery</Link></h3>
-                     </div>
-                     <div className='col-md-4 col-lg-4'>
-                        <h3><Link to ='/'>Returns and Refunds</Link></h3>
-                     </div>
-                     <div className='col-md-4 col-lg-4'>
-                        <h3><Link to ='/'>Products and Stock</Link></h3>
-                     </div>
-                     <div className='col-md-4 col-lg-4'>
-                        <h3><Link to ='/'>Technical Support</Link></h3>
-                     </div>
-                    
-                   </div>
+                 <div className="top-row flex-containers">
+                    <div className="">
+                        <img src={OrderIcon} className="img-fluid" alt="icon for order issues"/>
+                    </div>
+                    <div className="">2</div>
+                    <div className="">3</div>
+                 </div>
+                 <div className="bottom-row flex-containers">
+                 <div className="">1</div>
+                 <div className="">2</div>
+                 <div className="">3</div>
+                 </div>
                 </div>
             </React.Fragment>
         )
